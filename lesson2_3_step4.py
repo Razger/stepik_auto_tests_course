b0a961b5e4a9e70d139447ca9a924d6ee899d312
Бурдастиков Михаил Андреевич <m.burdastikov@atol.ru>
2024-01-09 15:42:11 +0300

    add file lessons

---
+++ lesson2_3_step4.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import pytest
+import time
+import math
+
+
+def test_dif():
+    try:
+        browser = webdriver.Chrome()
+        link = "http://suninjuly.github.io/alert_accept.html"
+        browser.get(link)
+
+        # Ваш код, который заполняет обязательные поля
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        confirm = browser.switch_to.alert
+        confirm.accept()
+
+        x_element = browser.find_element(By.ID, "input_value")
+        x = x_element.text
+
+        y = math.log(abs(12*math.sin(int(x))))
+
+        y_element = browser.find_element(By.ID, "answer")
+        y_element.send_keys(str(y))
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        # browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+        button.click()
+
+    finally:
+        # ожидание чтобы визуально оценить результаты прохождения скрипта
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
+
+# не забываем оставить пустую строку в конце файла
